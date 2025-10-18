cabccd9989f43adf5b040df2c81804fb47075310
whentheh-yt <mkacper1985@gmail.com>
2025-10-18 20:00:14 +0100

    Modify example

--- mods/example.py
+++ mods/example.py
@@ -8,17 +8,23 @@ If you want to submit your own custom mods,
 please contact the owner of this respository
 at whentheh.yt@gmail.com.
 """
+
+# Example: Run game with 2 windows
 import tkinter as tk
+import serpentes
+
+# ---- CONFIGURATION ----
+NUM_WINDOWS_MOD = 2 
+# -----------------------
+
+serpentes.NUM_WINDOWS = NUM_WINDOWS_MOD
+serpentes.cfg["game"]["num_windows"] = NUM_WINDOWS_MOD
+
 from serpentes import Serpentes
 
 def main():
     root = tk.Tk()
     game = Serpentes(root)
-
-    # Example: initiates windows 3 times
-    for _ in range(3):
-        game.init_windows()
-
     root.mainloop()
 
 if __name__ == "__main__":
